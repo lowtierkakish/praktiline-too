24439d54334df868892bb84b0486583994dace0b
lowtierkakish <cole77711@icloud.com>
2026-04-16 15:13:05 +0300

    feat: minor changes

--- frontend/src/app/(private)/layout.tsx
+++ frontend/src/app/(private)/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
       <main className="pt-16">
         {children}
       </main>
-      <div className="fixed bottom-3 right-4 text-xs text-gray-400 select-none">
+      <div className="fixed bottom-3 right-4 text-sm text-gray-400 select-none">
         Vilnevtsits Martin 11e
       </div>
     </div>
